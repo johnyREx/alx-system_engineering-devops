924fe10f4a4d14c4def4537e23db3a1fafefc8d9
johnyREx <johnkwofie99@icloud.com>
2023-11-25 02:00:17 -0800

    didaaadadiiidiii fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # Using puppet to install flusk
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  name     => 'flask',
+  provider => 'pip3'
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
